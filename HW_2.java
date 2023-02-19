3fc20d449816059834ffedaafe0efeed6ac4054f
Dmitriy-Born <1111born1111@gmail.com>
2023-02-19 19:58:09 +0300

    Ex_2

---
+++ HW_2.java
@@ -0,0 +1,19 @@
+public class HW_2 {
+    public static void main(String[] args) {
+        String str = "   Добрый день Как дела Все       хорошо      ";
+        System.out.println(point(str));
+    }
+    
+    public static String point (String str){
+        String newStr = "";
+        char p = '!';
+        newStr = str.trim().replaceAll("\s+", " ").
+                            replaceAll("\s([А-ЯЁ])", ". $1");
+        if (newStr.charAt(newStr.length()-1) != p){
+            newStr += p;
+        }
+
+        return newStr;
+    }
+
+}
